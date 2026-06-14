8ca62ada8bf2169324eba1fe6f3ebf63a219ab4e
Eldan Ben-Haim <eldan@benhaim.net>
2026-06-14 22:42:58 +0300

    lint and TS version update

--- src/parsers/WhitespaceParser.ts
+++ src/parsers/WhitespaceParser.ts
@@ -1,23 +1,26 @@
 import { ParseError, Parser, ParserContext, ParseResult } from "../core";
 
 export class WhitespaceParser implements Parser<void> {
-    constructor(private _mandatory: boolean) {
+  constructor(private _mandatory: boolean) {}
+
+  parse(parserContext: ParserContext) {
+    const input = parserContext.input;
+    let c;
+    let found = false;
+    while (
+      !input.eof() &&
+      ((c = input.peek(1)), c == " " || c == "\t" || c == "\n")
+    ) {
+      input.read(1);
+      found = true;
     }
 
-    parse(parserContext: ParserContext) {
-        const input = parserContext.input;
-        let c;
-        let found = false;
-        while(!input.eof() && (c = input.peek(1), c == ' ' ||  c == '\t' || c == '\n')) {
-            input.read(1);
-            found = true;
-        }
-
-        if(this._mandatory && !found) {
-            return ParseResult.failed(ParseError.parserRejected(this, parserContext));
-        } else {
-            return ParseResult.voidSuccessful();
-        }
+    if (this._mandatory && !found) {
+      return ParseResult.failed<void>(
+        ParseError.parserRejected(this, parserContext),
+      );
+    } else {
+      return ParseResult.voidSuccessful();
     }
-
+  }
 }
